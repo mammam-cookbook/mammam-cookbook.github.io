5606fa7559adfa56b92832433f24dd5ca3c51068
lminhtam <lminhtam99@gmail.com>
2021-02-27 12:57:03 +0700

    Updates

--- src/core/epic.js
+++
@@ -1,4 +0,0 @@
-import { authEpics } from 'pages/SignIn/redux/epics'
-import { combineEpics } from 'redux-observable'
-
-export const rootEpic = combineEpics(authEpics)
--- src/core/reducer.js
+++
@@ -1,9 +0,0 @@
-import { connectRouter } from 'connected-react-router'
-import { authReducer } from 'pages/SignIn/redux/reducer'
-import { combineReducers } from 'redux'
-import { history } from 'ultis/functions'
-
-export const rootReducer = combineReducers({
-  Auth: authReducer,
-  router: connectRouter(history)
-})
